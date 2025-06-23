9fa6cccdbf08dab03cb2602a9478855d91a72133
Ian Andrews <Ian.Andrews@ngc.com>
2025-06-23 05:29:17 -0400

    feat: rust dev image has more tools and is more DRY

--- toolbox.sh
+++ toolbox.sh
@@ -3,44 +3,76 @@
 # Start building a new container from archlinux
 container=$(sudo buildah from archlinux)
 
-# Allow man pages to be installed
-sudo buildah run $container -- sed -i '/usr\/share\/man/d' /etc/pacman.conf
+# Make this script a little more DRY
+function b_run() {
+  sudo buildah run \
+    -e RUSTUP_HOME=/build/multirust \
+    -e CARGO_HOME=/build/cargo \
+    -e http_proxy=$http_proxy \
+    -e https_proxy=$https_proxy \
+    $container -- "$@"
+}
+
+# Allow man pages to be installed when installing packages
+b_run sed -i '/usr\/share\/man/d' /etc/pacman.conf
 
 # Install packages
-sudo buildah run $container -- pacman -Suy --noconfirm \
+b_run pacman -Suy --noconfirm \
   bat \
   buildah \
+  cmake \
+  fd \
   fzf \
   gcc \
   git \
+  git-delta \
   jq \
   lynx \
   man-db \
   man-pages \
+  ninja \
   neovim \
   openssh \
+  openssl \
   pass \
+  pkgconf \
+  podman \
   ripgrep \
   rsync \
   runc \
   rustup \
+  sqlite \
   sudo \
   tmux \
   unzip \
+  which \
   zsh
 
 # Remove cached packages
-sudo buildah run $container -- bash -c 'yes | pacman -Scc'
+b_run bash -c 'yes | pacman -Scc'
+
+# Add everyone in the wheel group to sudo
+b_run echo '%wheel ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers
 
 # Enable US English locale and generate man pages
-sudo buildah run $container -- sed -ie 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
-sudo buildah run $container -- locale-gen
+b_run sed -ie 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
+b_run locale-gen
+
+# Create user
+b_run useradd -m -G wheel -s /usr/bin/zsh -u ${uid} ian
+
+# Setup /build
+b_run mkdir /build
+b_run chmod 0777 /build
+
+# Install one-off tools
+b_run bash -c 'curl -sL https://ziglang.org/download/0.13.0/zig-linux-x86_64-0.13.0.tar.xz | tar -Jx -C /build/'
 
 # Install rust
-sudo buildah run $container -- mkdir -p /build/multirust
-sudo buildah run $container -- mkdir -p /build/cargo
-sudo buildah run -e RUSTUP_HOME=/build/multirust $container -- rustup install stable
-sudo buildah run -e RUSTUP_HOME=/build/multirust -e CARGO_HOME=/build/cargo $container -- cargo install --locked cargo-watch jj-cli cargo-audit
+b_run mkdir -p /build/multirust
+b_run mkdir -p /build/cargo
+b_run rustup install stable
+b_run cargo install --locked cargo-watch jj-cli cargo-audit cargo-checkmate licensure
 
 # Commit the toolbox image
 sudo buildah commit $container toolbox
